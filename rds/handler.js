79ba06688c73d3434f5b8cacd07e2f20fce3b9d7
jnpark <jnpark@pentasecurity.com>
2018-12-04 06:21:51 +0900

    [serverless graphql setting]

--- rds/handler.js
+++ rds/handler.js
@@ -1,16 +1,20 @@
-'use strict';
+import { graphqlLambda, graphiqlLambda } from 'apollo-server-lambda';
+import { makeExecutableSchema } from 'graphql-tools';
+import { schema } from './schema';
+import { resolvers } from './resolvers';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const myGraphQLSchema = makeExecutableSchema({
+  typeDefs: schema,
+  resolvers,
+});
 
-  callback(null, response);
+exports.graphqlHandler = function graphqlHandler(event, context, callback) {
+  function callbackWithHeaders(error, output) {
+    // eslint-disable-next-line no-param-reassign
+    output.headers['Access-Control-Allow-Origin'] = '*';
+    callback(error, output);
+  }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  const handler = graphqlLambda({ schema: myGraphQLSchema });
+  return handler(event, context, callbackWithHeaders);
 };
